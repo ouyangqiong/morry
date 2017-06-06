43455569158e2a54b89689ed59f2c22e06ed81d8
ouyangqiong <no email>
2017-06-07 07:53:23 +0800

    my first commit

---
+++ morry/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for morry project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "morry.settings")
+
+application = get_wsgi_application()
